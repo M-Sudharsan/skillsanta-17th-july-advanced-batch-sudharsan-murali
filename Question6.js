701c1673d6e0728bb2978c529fa2754e280ee3c9
SUDHARSAN MURALI <40484760+M-Sudharsan@users.noreply.github.com>
2021-08-03 14:54:47 +0530

    Question 6/10

--- Question6.js
+++ Question6.js
@@ -1,1 +1,15 @@
+/*
+Q6.
+const set = new Set(['Beethoven', 'Mozart', 'Chopin', 'Chopin'])
+How to delete 'Beethoven' from set
+*/
 
+
+const set = new Set(['Beethoven', 'Mozart', 'Chopin', 'Chopin'])
+console.log(set.delete('Beethoven'));
+
+/*
+OUTPUT:
+
+> true
+*/
