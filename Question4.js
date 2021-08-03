b8d1d0b78878868d265935cfa035f19c248e4996
SUDHARSAN MURALI <40484760+M-Sudharsan@users.noreply.github.com>
2021-08-03 14:25:41 +0530

    Question 4/10
    Complicated. But learnt something new and interesting.

--- Question4.js
+++ Question4.js
@@ -1,1 +1,59 @@
+/*
 
+Q4. Consider the two functions below. Will they both return the same thing? Why or why
+not?
+  
+  
+  function foo1()
+  {
+  return {
+  bar: "hello"
+  };
+  }
+  function foo2()
+  {return
+  {
+  bar: "hello"
+  };
+  }
+
+*/
+
+function foo1()
+{
+return {
+bar: "hello"
+};
+}
+function foo2()
+{return
+{
+bar: "hello"
+};
+}
+
+
+/*
+
+OUTPUT:
+// [object Object] 
+{
+  "bar": "hello"
+}
+
+undefined
+
+
+ANSWER: No. 
+EXPLANATION: The placement of the curly braces '{' or '}' causes a change in the output.
+This is mostly because of two reasons -- The semicolon convention and the curly brace convention is Js.
+Briefly, the semicolon convention is optional in Js. But not using it is considered as  bad form. 
+If a semicolon is not specified, Js assumes a semi colon at the end of the line.
+
+Now, the placement of the curly braces plays a major part in this. 
+If the curly brace is placed at the end of the line before returning the line without the semicolon,the code is perfectly valid and gives a proper output.
+Now, since the curly brace is placed at the beginning of the new line and immediately returned before calling the return statement, 
+Js automatically assumes a semicolon at the end of the new line i.e., immediately after the '{'. This means that the code after the '{' is not considered. 
+
+This is why, foo1 returns the value while foo2 returns as 'undefined'
+*/
